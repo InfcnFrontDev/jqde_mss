61bb13f52763d151db296dba3ed645b3701268cb
wangp <wangp@infcn.com.cn>
2016-11-14 18:35:23 +0800

    fg

--- app/qdeMods/qdeReports/main.js
+++ app/qdeMods/qdeReports/main.js
@@ -1,1 +1,34 @@
 
+var vmApp = new Vue({
+    el: '#vmApp',
+    data: {
+        menus: []
+    },
+    mounted: function () {
+        this.fetchData();
+
+    },
+    methods: {
+        fetchData: function () {
+            var $this = this;
+            JqdeMods.ajax('qdeReports', 'getReports').then(function (result) {
+                if (result.success) {
+                    $this.render(result);
+                }
+            }, function (error) {
+                console.log(error);
+            });
+        },
+        render: function (result) {
+            var menus = []
+            for (var i in result.rows) {
+                var moduleMenu = {
+                    name: result.rows[i].key,
+                    value: result.rows[i].value
+                };
+                menus.push(moduleMenu);
+            }
+            this.menus = menus;
+        }
+    }
+})
