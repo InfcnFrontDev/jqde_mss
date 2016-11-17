f11162805336a54c2b6a97185547eac1e66e8f47
guoxm <guoxm@infcn.com.cn>
2016-11-17 15:51:31 +0800

    44444

--- app/modules/qdeAdmin/main.js
+++ app/modules/qdeAdmin/main.js
@@ -2,6 +2,12 @@ var vmApp = new Vue({
     el: '#vmApp',
     data: {
         home: [],
+        name:'',
+        phone:'12121212',
+        password:'',
+        department:'',
+        user:'',
+        email:'',
         checkedall: false,
         checked: false,
 
@@ -9,6 +15,7 @@ var vmApp = new Vue({
     mounted: function () {
         this.fetchData();
 
+        var $this=this;
         $("#bootbox-confirm").on(ace.click_event, function() {
             $.get('modules/qdeAdmin/information.html', function (html) {
                 bootbox.dialog({
@@ -16,17 +23,26 @@ var vmApp = new Vue({
                     title: "<span class='bigger-110'>添加管理员</span>",
                     buttons: {
                         cancel: {
-                            label: '<i class="fa fa-times"></i> 取消'
+                            label: '<i class="fa fa-times"></i> 取消',
+                            "callback": function () {
+                                //Example.show("great success");
+
+                            }
                         },
                         confirm: {
-                            label: '<i class="fa fa-check"></i> 保存'
-                        }
+                            label: '<i class="fa fa-check"></i> 保存',
+                            "callback": function () {
+                            //Example.show("great success");
+                            $this.addinformation()
 
+                            }
+                        }
                     }
 
                 })
             })
         });
+
 
     },
     methods: {
@@ -40,15 +56,104 @@ var vmApp = new Vue({
 
             });
         },
-        checked: function () {
-            if (this.checkedall == false) {
-                this.checkedall = true;
-                this.checked = true;
-            } else {
-
+        addinformation:function(){
+            var $this=this
+           /* if(vmadd.checked){
+                var $tr="<tr>\
+                <td class=\"center\">\
+                <label class=\"pos-rel\">\
+                <input type=\"checkbox\" class=\"ace checkadmin\" v-model=\"checked\"/>\
+                <span class=\"lbl\"></span>\
+                </label>\
+                </td>\
+                <td >\
+                <a href=\"#\">"+vmadd.userid+"</a>\
+            </td>\
+            <td >"+vmadd.name+"</td>\
+            <td class=\"hidden-480\">"+vmadd.picked+"</td>\
+            <td>"+vmadd.phone+"</td>\
+            <td class=\"hidden-480\">"+vmadd.email+"</td>\
+            <td>"+vmadd.department+"</td>\
+            <td>\
+            <div class=\"hidden-sm hidden-xs action-buttons\">\
+                <a class=\"green\" href=\"#\">\
+                <i class=\"ace-icon fa fa-pencil  bigger-130\"></i>\
+                </a>\
+                <a class=\"red\" href=\"#\">\
+                <i class=\"ace-icon fa fa-trash-o  bigger-130\"></i>\
+                </a>\
+                </div>\
+                </td>\
+                <td>\
+                <div class=\"hidden-sm hidden-xs action-buttons\">\
+                <a class=\"green\" href=\"#\">\
+                <i class=\"ace-icon fa fa-check bigger-130\"></i>\
+                </a>\
+                </div>\
+                </td>\
+                </tr>"
+            }else{
+                var $tr="<tr>\
+                <td class=\"center\">\
+                <label class=\"pos-rel\">\
+                <input type=\"checkbox\" class=\"ace checkadmin\" v-model=\"checked\"/>\
+                <span class=\"lbl\"></span>\
+                </label>\
+                </td>\
+                <td >\
+                <a href=\"#\">"+vmadd.userid+"</a>\
+            </td>\
+            <td >"+vmadd.name+"</td>\
+            <td class=\"hidden-480\">"+vmadd.picked+"</td>\
+            <td>"+vmadd.phone+"</td>\
+            <td class=\"hidden-480\">"+vmadd.email+"</td>\
+            <td>"+vmadd.department+"</td>\
+            <td>\
+            <div class=\"hidden-sm hidden-xs action-buttons\">\
+                <a class=\"green\" href=\"#\">\
+                <i class=\"ace-icon fa fa-pencil  bigger-130\"></i>\
+                </a>\
+                <a class=\"red\" href=\"#\">\
+                <i class=\"ace-icon fa fa-trash-o  bigger-130\"></i>\
+                </a>\
+                </div>\
+                </td>\
+                <td>\
+                <div class=\"hidden-sm hidden-xs action-buttons\">\
+                <a class=\"red\" href=\"#\">\
+                <i class=\"ace-icon glyphicon glyphicon-remove bigger-130\"></i>\
+                </a>\
+                </div>\
+                </td>\
+                </tr>"
+            }*/
+            if(vmadd.checked){
+                var arr={
+                    userId:vmadd.userid,
+                    name:vmadd.name,
+                    picked:vmadd.picked,
+                    phone:vmadd.phone,
+                    email:vmadd.email,
+                    enabled:true
+                };
+            }else{
+                var arr={
+                    userId:vmadd.userid,
+                    name:vmadd.name,
+                    picked:vmadd.picked,
+                    phone:vmadd.phone,
+                    email:vmadd.email,
+                    enabled:false
+                };
             }
+            console.log($this.home);
+            $this.home.push(arr)
+            console.log($this.home);
 
 
+
+                /*  var addtr=$tr;
+                $('.infor-tbody').append(addtr)*/
         },
         render: function (result) {
 
